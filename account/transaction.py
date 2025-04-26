25a6610896a5128f568fcb87742dfd587b014a29
IsuruNimantha141 <devilshunetr56@gmail.com>
2025-04-26 12:11:32 +0530

    Update transaction.py

--- account/transaction.py
+++ account/transaction.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 class Transaction:
-    def __init__(self, amount, transaction_type):
+ def __init__(self, amount, transaction_type):
         self.amount = amount
-        self.transaction_type = transaction_type  # "deposit" or "withdraw"
+       if transaction_type not in ["deposit", "withdraw"]:
         self.timestamp = datetime.now()
 
     def __str__(self):
