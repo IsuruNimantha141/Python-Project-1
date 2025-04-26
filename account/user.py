019db429fe30e98a22c20d018b2259e91ed937fb
IsuruNimantha141 <devilshunetr56@gmail.com>
2025-04-26 12:19:28 +0530

    Update user.py

--- account/user.py
+++ account/user.py
@@ -1,13 +1,13 @@
 class User:
-    def __init__(self, name, email):
-        self.name = name
+  def __init__(self, name, email):
+       self.name = name
         self.email = email
         self.accounts = []
 
     def add_account(self, account):
         self.accounts.append(account)
 
-    def get_total_balance(self): 
+  def get_total_balance(self):
         return 0
 
     def get_account_count(self):
@@ -18,9 +18,9 @@ class User:
     def remove_account(self, account):
         return "Account"
 
-    def is_valid_email(self,email):
+ def is_valid_email(self, email):
         return None
 
 
-    def __str__(self):
-        return f"{self.name} ({self.email}) - {self.get_account_count()} account(s), Total Balance: ${self.get_total_balance()}"
+ def __str__(self):
+              return f"{self.name} ({self.email}) - {self.get_account_count()} account(s), Total Balance: ${self.get_total_balance():.2f}"
